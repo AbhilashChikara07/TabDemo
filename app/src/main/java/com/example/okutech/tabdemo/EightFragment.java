3db1977ea1804464f89e24f2022e71debd321919
Abhilash Chikara <abhilash.chikara@fastfox.com>
2017-09-05 11:51:26 +0530

    first commit

---
+++ app/src/main/java/com/example/okutech/tabdemo/EightFragment.java
@@ -0,0 +1,27 @@
+package com.example.okutech.tabdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Description
+ *
+ * @author Abhilash Chikara
+ * @version 1.0
+ * @since 9/4/17
+ */
+
+public class EightFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view, container, false);
+        return view;
+    }
+}
+
